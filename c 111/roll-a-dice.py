50b8105a6f763986ae644f7c77814865ff2b1369
saanviGarg01 <158058912+saanviGarg01@users.noreply.github.com>
2024-05-27 20:00:23 +0530

    Add files via upload

---
+++ c 111/roll-a-dice.py
@@ -0,0 +1,53 @@
+import random
+response = "y"
+
+while  response == "y" :
+
+   
+   no=random.randint(1,6)
+   if no == 1:
+          print("[-------]")
+          print("[-------]") 
+          print("[   1    ]")
+          print("[-------]")
+          print("[-------]")
+
+   if no == 2:
+          print("[-------]")
+          print("[-------]") 
+          print("[   2   ]")
+          print("[-------]")
+          print("[-------]")
+
+   if no == 3:
+          print("[-------]")
+          print("[-------]") 
+          print("[   3    ]")
+          print("[-------]")
+          print("[-------]")
+
+   if no == 4:
+          print("[-------]")
+          print("[-------]") 
+          print("[  4    ]")
+          print("[-------]")
+          print("[-------]")
+
+   if no == 6:
+          print("[-------]")
+          print("[-------]") 
+          print("[   6    ]")
+          print("[-------]")
+          print("[-------]")
+
+   if no == 5:
+          print("[-------]")
+          print("[-------]") 
+          print("[   5    ]")
+          print("[-------]")
+          print("[-------]")
+
+response=input("pre y to continue and no to exit")
+print("\n")
+
+
